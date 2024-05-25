75771d96067feefc78d6bfcf69cc05da8087b277
Ayaan <javedayaan06@gmail.com>
2024-05-25 19:29:26 +0530

    ex 9 done

---
+++ 1. Fixed Size Data/Excercise 1.bak
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Excercise 1|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define x 3)
+(define y 4)
+(expt (+ (expt x 2) (expt y 2)) 0.5)
+;tells distance of (x,y) from origin
---
+++ 1. Fixed Size Data/Excercise 9.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Excercise 9|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define (input in)
+  (cond
+    [(string? in) (string-length in)]
+    [(image? in) (* (image-height in) (image-width in))]
+    [(number? in) (expt (sqr in) 0.5)]))

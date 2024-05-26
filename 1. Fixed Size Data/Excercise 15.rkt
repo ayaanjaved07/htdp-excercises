4d8c7c01bb7e88dd94e6aca95d77140706f72855
Ayaan <javedayaan06@gmail.com>
2024-05-26 21:12:42 +0530

    hmm

---
+++ 1. Fixed Size Data/Excercise 15.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Excercise 15|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define sunny #false)
+(define friday #true)
+
+(define (f x) (or x #false))
+;Define ==>. The function consumes two Boolean values, call them sunny and friday. Its answer is #true if sunny is false or friday is true.
+  
